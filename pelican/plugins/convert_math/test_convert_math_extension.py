33b957f3d08fc7679f94331924d8ef208ef7954b
Laszlo Treszkai <laszlo.treszkai@gmail.com>
2020-10-26 17:10:13 +0100

    test: add display math tests

--- pelican/plugins/convert_math/test_convert_math_extension.py
+++ pelican/plugins/convert_math/test_convert_math_extension.py
@@ -1,23 +1,74 @@
 from markdown.test_tools import TestCase
 
+from pelican.plugins.convert_math import PelicanMathExtension
 
-class TestHr(TestCase):
-    def test_hr_before_paragraph(self):
+
+class TestMathExtension(TestCase):
+    def test_math_inline(self):
         self.assertMarkdownRenders(
             # The Markdown source text used as input
             self.dedent(
-                """
-                ***
-                An HR followed by a paragraph with no blank line.
+                r"""
+                Foo \( bar _baz_ qux \),
+                lorem $a_b$ ipsum $4.00 $5.00,
+                `do $not$ parse`
                 """
             ),
             # The expected HTML output
             self.dedent(
-                """
-                <hr>
-                <p>An HR followed by a paragraph with no blank line.</p>
+                r"""
+                <p>Foo <script type="math/tex"> bar _baz_ qux </script>,
+                lorem <script type="math/tex">a_b</script> ipsum $4.00 $5.00,
+                <code>do $not$ parse</code></p>
                 """
             ),
             # Other keyword arguments to pass to `markdown.markdown`
             output_format='html',
+            extensions=[PelicanMathExtension()],
+        )
+
+    def test_math_display_square(self):
+        self.assertMarkdownRenders(
+            # The Markdown source text used as input
+            self.dedent(
+                r"""
+                \[Foo _bar_ baz;
+                `even this is copy-paste`
+                \]
+                """
+            ),
+            # The expected HTML output
+            self.dedent(
+                r"""
+                <script type="math/tex; mode=display">% <![CDATA[
+                Foo _bar_ baz;
+                `even this is copy-paste` %]]></script>
+                """
+            ),
+            # Other keyword arguments to pass to `markdown.markdown`
+            output_format='html',
+            extensions=[PelicanMathExtension()],
+        )
+
+    def test_math_display_dollars(self):
+        self.assertMarkdownRenders(
+            # The Markdown source text used as input
+            self.dedent(
+                r"""
+                $$Foo _bar_ baz;
+                `even this is copy-paste`
+                $$
+                """
+            ),
+            # The expected HTML output
+            self.dedent(
+                r"""
+                <script type="math/tex; mode=display">% <![CDATA[
+                Foo _bar_ baz;
+                `even this is copy-paste` %]]></script>
+                """
+            ),
+            # Other keyword arguments to pass to `markdown.markdown`
+            output_format='html',
+            extensions=[PelicanMathExtension()],
         )
